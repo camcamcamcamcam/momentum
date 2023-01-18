5722d0d3d5c3e268ebb5905976e37c4181f512f9
Cameron <eboysix@hotmail.co.nz>
2023-01-18 17:36:40 +1300

    Consolidated classes
    And fixed some mappings I forgot to do
    And removed some experiments
    And fixed the breaking sound happening all the time

--- src/main/java/com/minecraftabnormals/momentum/Momentum.java
+++ src/main/java/com/minecraftabnormals/momentum/Momentum.java
@@ -1,12 +1,10 @@
-package com.minecraftabnormals.momentum.core;
+package com.minecraftabnormals.momentum;
 
-import com.minecraftabnormals.abnormals_core.common.world.storage.tracking.DataProcessors;
-import com.minecraftabnormals.abnormals_core.common.world.storage.tracking.TrackedData;
 import com.minecraftabnormals.abnormals_core.common.world.storage.tracking.TrackedDataManager;
 import com.minecraftabnormals.abnormals_core.core.registry.LootInjectionRegistry;
 import com.minecraftabnormals.abnormals_core.core.util.registry.RegistryHelper;
 import com.minecraftabnormals.abnormals_core.core.util.registry.SoundSubRegistryHelper;
-import com.minecraftabnormals.momentum.core.registry.Enchantments;
+import net.minecraft.enchantment.Enchantment;
 import net.minecraft.loot.LootTables;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundEvent;
@@ -15,8 +13,8 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-
-import javax.sound.midi.Track;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
 
 @Mod(Momentum.MODID)
 @Mod.EventBusSubscriber(modid = Momentum.MODID)
@@ -27,22 +25,20 @@ public class Momentum {
     public static final RegistryHelper REGISTRY_HELPER = new RegistryHelper(MODID);
 
     public static final SoundSubRegistryHelper SOUND_HELPER = REGISTRY_HELPER.getSoundSubHelper();
+    public static final DeferredRegister<Enchantment> ENCHANTMENTS = DeferredRegister.create(ForgeRegistries.ENCHANTMENTS, Momentum.MODID);
 
-    public static final TrackedData<Integer> BLOCKS_MINED = TrackedData.Builder.create(DataProcessors.INT, () -> 0).enableSaving().build();
-    public static final TrackedData<ResourceLocation> LAST_BLOCK = TrackedData.Builder.create(DataProcessors.RESOURCE_LOCATION, () -> new ResourceLocation("air")).enableSaving().build();
-    public static final TrackedData<Boolean> SOUND_PLAYED = TrackedData.Builder.create(DataProcessors.BOOLEAN, () -> false).enableSaving().build();
-
+    public static final RegistryObject<Enchantment> MOMENTUM = ENCHANTMENTS.register("momentum", MomentumEnchantment::new);
     public static final RegistryObject<SoundEvent> MOMENTUM_HALT = SOUND_HELPER.createSoundEvent("item.momentum.halt");
 
     public Momentum() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         REGISTRY_HELPER.getSoundSubHelper().getDeferredRegister().register(modEventBus);
-        Enchantments.ENCHANTMENTS.register(modEventBus);
+        ENCHANTMENTS.register(modEventBus);
         MinecraftForge.EVENT_BUS.register(this);
 
-        TrackedDataManager.INSTANCE.registerData(new ResourceLocation(Momentum.MODID, "blocks_mined"), BLOCKS_MINED);
-        TrackedDataManager.INSTANCE.registerData(new ResourceLocation(Momentum.MODID, "last_block"), LAST_BLOCK);
-        TrackedDataManager.INSTANCE.registerData(new ResourceLocation(Momentum.MODID, "sound_played"), SOUND_PLAYED);
+        TrackedDataManager.INSTANCE.registerData(new ResourceLocation(Momentum.MODID, "blocks_mined"), MomentumEnchantment.BLOCKS_MINED);
+        TrackedDataManager.INSTANCE.registerData(new ResourceLocation(Momentum.MODID, "last_block"), MomentumEnchantment.LAST_BLOCK);
+        TrackedDataManager.INSTANCE.registerData(new ResourceLocation(Momentum.MODID, "sound_played"), MomentumEnchantment.SOUND_PLAYED);
 
         LootInjectionRegistry.LootInjector injector = new LootInjectionRegistry.LootInjector(Momentum.MODID);
         injector.addLootInjection(injector.buildLootPool("abandoned_mineshaft", 1, 0), LootTables.ABANDONED_MINESHAFT);
---
+++ src/main/java/com/minecraftabnormals/momentum/MomentumEnchantment.java
@@ -0,0 +1,123 @@
+package com.minecraftabnormals.momentum;
+
+import com.minecraftabnormals.abnormals_core.common.world.storage.tracking.DataProcessors;
+import com.minecraftabnormals.abnormals_core.common.world.storage.tracking.IDataManager;
+import com.minecraftabnormals.abnormals_core.common.world.storage.tracking.TrackedData;
+import net.minecraft.enchantment.EfficiencyEnchantment;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.enchantment.EnchantmentType;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundCategory;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.event.world.BlockEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = Momentum.MODID)
+public class MomentumEnchantment extends Enchantment {
+
+    public static final TrackedData<Integer> BLOCKS_MINED = TrackedData.Builder.create(DataProcessors.INT, () -> 0).enableSaving().build();
+    public static final TrackedData<ResourceLocation> LAST_BLOCK = TrackedData.Builder.create(DataProcessors.RESOURCE_LOCATION, () -> new ResourceLocation("air")).enableSaving().build();
+    public static final TrackedData<Boolean> SOUND_PLAYED = TrackedData.Builder.create(DataProcessors.BOOLEAN, () -> false).enableSaving().build();
+
+    public MomentumEnchantment() {
+        super(Rarity.RARE, EnchantmentType.DIGGER, new EquipmentSlotType[]{EquipmentSlotType.MAINHAND});
+    }
+
+    @Override
+    public boolean checkCompatibility(Enchantment ench) {
+        return !(ench instanceof EfficiencyEnchantment) && super.checkCompatibility(ench);
+    }
+
+    @Override
+    public boolean isTreasureOnly() {
+        return true;
+    }
+
+    @SubscribeEvent
+    public static void getBreakSpeed(PlayerEvent.BreakSpeed event) {
+        ResourceLocation currentBlock = event.getState().getBlock().getRegistryName();
+        PlayerEntity player = event.getPlayer();
+        IDataManager playerManager = ((IDataManager) player);
+        float hardness = event.getState().getDestroySpeed(player.level, event.getPos());
+
+        if (EnchantmentHelper.getItemEnchantmentLevel(Momentum.MOMENTUM.get(), player.getMainHandItem()) > 0
+                && playerManager.getValue(LAST_BLOCK).toString().equals(currentBlock.toString()))
+        {
+            float speedFactor = (float) Math.pow(Math.pow(2, -1.0 / 16 * hardness + 3.0 / 16) + 1, playerManager.getValue(BLOCKS_MINED) + 1);
+            if (playerManager.getValue(BLOCKS_MINED) + 1 >= 8 * Math.sqrt(hardness)) {
+                speedFactor = Math.min(22 * hardness / player.getMainHandItem().getDestroySpeed(event.getState()), speedFactor);
+                if (!playerManager.getValue(SOUND_PLAYED)) {
+                    event.getPlayer().level.playSound(player, event.getPos(), Momentum.MOMENTUM_HALT.get(), SoundCategory.PLAYERS, 0.2f, 1.0f);
+                    playerManager.setValue(SOUND_PLAYED, true);
+                }
+            }
+            event.setNewSpeed(event.getOriginalSpeed() * speedFactor);
+        }
+    }
+
+    @SubscribeEvent
+    public static void onBlockDestroy(BlockEvent.BreakEvent event) {
+        ResourceLocation currentBlock = event.getState().getBlock().getRegistryName();
+        PlayerEntity player = event.getPlayer();
+        IDataManager playerManager = ((IDataManager) player);
+        boolean isBlockInstaminable = event.getState().getDestroySpeed(player.level, event.getPos()) == 0;
+
+        if (EnchantmentHelper.getItemEnchantmentLevel(Momentum.MOMENTUM.get(), player.getMainHandItem()) > 0
+                && playerManager.getValue(LAST_BLOCK).toString().equals(currentBlock.toString())) {
+            playerManager.setValue(BLOCKS_MINED, playerManager.getValue(BLOCKS_MINED) + 1);
+        } else if (!isBlockInstaminable) {
+            playerManager.setValue(BLOCKS_MINED, 0);
+            playerManager.setValue(SOUND_PLAYED, false);
+        }
+        if (!isBlockInstaminable) {
+            playerManager.setValue(LAST_BLOCK, currentBlock);
+        }
+    }
+
+    /*
+    @SubscribeEvent
+    public static void onPlayerTick(TickEvent.PlayerTickEvent event) {
+
+        if (event.phase == TickEvent.Phase.START)
+            return;
+
+        PlayerEntity player = event.player;
+        IDataManager playerManager = ((IDataManager) player);
+
+        EffectInstance haste = player.getEffect(Effects.DIG_SPEED);
+        int hasteLevel = -1;
+        if (haste != null)
+            hasteLevel = haste.getAmplifier();
+
+
+        double disX = player.getX() - player.xOld;
+        double disZ = player.getZ() - player.zOld;
+
+        if (player.getDeltaMovement().length() == 0.0 || disX == 0.0 && disZ == 0.0) {
+            return;
+        }
+
+        if (player.isShiftKeyDown())
+            return;
+        if (!player.isSprinting() && hasteLevel >= 0)
+            return;
+        if (player.isSprinting() && hasteLevel >= 1)
+            return;
+
+        if (playerManager.getValue(BLOCKS_MINED) == 0)
+            return;
+
+        playerManager.setValue(BLOCKS_MINED, 0);
+        playerManager.setValue(SOUND_PLAYED, false);
+        player.level.playSound(player, player.blockPosition(), Momentum.MOMENTUM_HALT.get(), SoundCategory.PLAYERS, 0.2f, 1.0f);
+
+    }*/
+
+}
